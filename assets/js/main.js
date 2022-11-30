d72047b46ca75ac6bf8ba73fd97f51b16b18f793
daniloboccomino <daniloboccomino@gmail.com>
2022-11-30 19:46:45 -0300

    mobile navbar button

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,3 +13,18 @@ window.onscroll = function () {
         header_navbar.classList.remove('sticky')
     }
 }
+
+document.querySelectorAll('.toggleNav').forEach(nav => nav.addEventListener('click', () => {
+    btnNav = document.querySelector('.navbar-toggler')
+    if (!window.matchMedia('(min-width: 992px)').matches) {
+        if (btnNav.classList.contains('collapsed')) {
+            btnNav.classList.remove('collapsed')
+            btnNav.setAttribute('aria-expanded', true)
+            document.querySelector('#navbarResponsive').classList.add('show')
+        } else {
+            btnNav.classList.add('collapsed')
+            btnNav.setAttribute('aria-expanded', false)
+            document.querySelector('#navbarResponsive').classList.remove('show')
+        }
+    }
+}))
